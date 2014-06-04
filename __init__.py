5ea6ecac2b19bf6df7dedecf4837d87437d6d62b
Leandro Ezequiel Baldi <baldileandro@gmail.com>
2014-06-04 12:13:36 -0300

    Add
    overwrite get_next_invoice_number

--- __init__.py
+++ __init__.py
@@ -17,5 +17,5 @@
 ##############################################################################
 
 import pfiscal
-
+import invoice
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
